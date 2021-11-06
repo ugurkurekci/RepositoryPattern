466b7b812eb2eb27cf63e0c0c59391de9f40aa7e
UĞUR KÜREKCİ <ugurkurekci98@hotmail.com>
2021-11-06 18:43:54 +0300

    push :)

--- Business/Concrete/CityService.cs
+++ Business/Concrete/CityService.cs
@@ -28,27 +28,29 @@ namespace Business.Concrete
 
         public IResult Delete(City city)
         {
-            throw new NotImplementedException();
+            _cityDal.Delete(city);
+            return new SuccessResult(AllMessages.CityDeleted);
         }
 
         public IDataResult<List<City>> GetAll()
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<List<City>>(_cityDal.GetAll(), AllMessages.CityListed);
         }
 
         public IDataResult<City> GetCityById(int cityId)
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<City>(_cityDal.Get(p => p.CityId == cityId), AllMessages.CityById);
         }
 
         public IDataResult<City> GetCityByName(string cityName)
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<City>(_cityDal.Get(p => p.CityName == cityName), AllMessages.CityByName);
         }
 
         public IResult Update(City city)
         {
-            throw new NotImplementedException();
+            _cityDal.Update(city);
+            return new SuccessResult(AllMessages.CityUpdated);
         }
     }
 }
