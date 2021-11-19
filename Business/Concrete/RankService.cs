8b4ee462a23f7a43750638eb99a5b531b630c0e1
UĞUR KÜREKCİ <ugurkurekci98@hotmail.com>
2021-11-19 14:39:38 +0300

    Rank Deparment operation added

---
+++ Business/Concrete/RankService.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RankService : IRankService
+    {
+        private readonly IRankDal _rankDal;
+
+        public RankService(IRankDal rankDal)
+        {
+            _rankDal = rankDal;
+        }
+
+        public IResult Add(Rank rank)
+        {
+            _rankDal.Add(rank);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Rank rank)
+        {
+            _rankDal.Delete(rank);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rank>> GetAll()
+        {
+            return new SuccessDataResult<List<Rank>>(_rankDal.GetAll());
+        }
+
+        public IDataResult<Rank> GetRankById(int rankId)
+        {
+            return new SuccessDataResult<Rank>(_rankDal.Get(o => o.Rankıd == rankId));
+        }
+
+        public IDataResult<Rank> GetRankByName(string rankName)
+        {
+            return new SuccessDataResult<Rank>(_rankDal.Get(x => x.RankName == rankName));
+        }
+
+        public IResult Update(Rank rank)
+        {
+            _rankDal.Update(rank);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ WebApi/Controllers/RankController.cs
@@ -0,0 +1,92 @@
+﻿using Business.Abstract;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankController : ControllerBase
+    {
+        private readonly IRankService _rankService;
+
+        public RankController(IRankService rankService)
+        {
+            _rankService = rankService;
+        }
+
+        [HttpPost("rankAdd")]
+        public IActionResult RankAdd(Rank rank)
+        {
+            var result = _rankService.Add(rank);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+        [HttpPut("rankUpdate")]
+        public IActionResult RankUpdate(Rank rank)
+        {
+            var result = _rankService.Update(rank);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpDelete("rankDelete")]
+        public IActionResult RankDelete(Rank rank)
+        {
+            var result = _rankService.Delete(rank);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpGet("rankGetAll")]
+        public IActionResult RankGetAll()
+        {
+            var result = _rankService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+        [HttpGet("rankById")]
+        public IActionResult RankById(int id)
+        {
+            var result = _rankService.GetRankById(id);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+        [HttpGet("rankName")]
+        public IActionResult RankByDescription(string rankName)
+        {
+            var result = _rankService.GetRankByName(rankName);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+    }
+}
