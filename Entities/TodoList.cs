3ea42fc4d165ece432706422be9a189664df748f
UĞUR KÜREKCİ <ugurkurekci98@hotmail.com>
2021-11-19 17:24:10 +0300

    To-do list integrated.

---
+++ Entities/TodoList.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class TodoList : IEntity
+    {
+        public int Id { get; set; }
+        public string ToDoName { get; set; }
+        public string ToDoDescription { get; set; }
+        public Boolean ToDoIsItDone { get; set; }
+    }
+}
