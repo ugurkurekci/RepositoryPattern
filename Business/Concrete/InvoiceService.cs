de54d27709f770dab94ca5df8f3dd56a143f15f6
UĞUR KÜREKCİ <ugurkurekci98@hotmail.com>
2021-11-19 14:31:18 +0300

    Invoice record integrated.

---
+++ Business/Concrete/InvoiceService.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IInvoiceDal _invoiceDal;
+
+        public InvoiceService(IInvoiceDal invoiceDal)
+        {
+            _invoiceDal = invoiceDal;
+        }
+
+        public IResult Add(Invoice invoice)
+        {
+            _invoiceDal.Add(invoice);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Invoice invoice)
+        {
+            _invoiceDal.Delete(invoice);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Invoice>> GetAll()
+        {
+            return new SuccessDataResult<List<Invoice>>(_invoiceDal.GetAll());
+        }
+
+        public IDataResult<Invoice> GetInvoiceByDate(DateTime InvoiceDate)
+        {
+            return new SuccessDataResult<Invoice>(_invoiceDal.Get(p => p.InvoiceDate == InvoiceDate));
+        }
+
+        public IDataResult<Invoice> GetInvoiceById(int InvoiceId)
+        {
+            return new SuccessDataResult<Invoice>(_invoiceDal.Get(p => p.InvoiceId == InvoiceId));
+        }
+
+        public IDataResult<Invoice> GetInvoiceByName(string InvoiceName)
+        {
+            return new SuccessDataResult<Invoice>(_invoiceDal.Get(p => p.InvoiceName == InvoiceName));
+        }
+
+        public IResult Update(Invoice invoice)
+        {
+            _invoiceDal.Update(invoice);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ Entities/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Invoice : IEntity
+    {
+        public int InvoiceId { get; set; }
+        public string InvoiceName { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime LastInvoiceDate { get; set; }
+        public double InvoicePrice { get; set; }
+        public Boolean InvoiceIfBoolean { get; set; }
+
+
+    }
+}
